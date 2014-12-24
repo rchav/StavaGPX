bd5f2adb10d97d28c74432b71c521628e83d3a19
Robert Chavez <robert.chavez@me.com>
2014-12-24 09:30:51 -0500

    some columns in dataframe

--- run.py
+++ run.py
@@ -5,17 +5,30 @@ filename = '20130223-233104-Ride.gpx'
 points =[]
 recordID = []
 count = 0
+lats = []
+longs = []
 
 with open(filename, 'r') as file:
 	for line in file:
 		cleanLine = line.strip()
 		if cleanLine[:6] == "<trkpt":
-			points.append(line)
+			points.append(cleanLine)
 			count = count + 1
 			recordID.append(count)
 
-df = pd.DataFrame(columns = ['Record', 'Trackpoint', 'Lat', 'Long', 'Elevation', 'Date'])
-df['Record'] = recordID
+			cleanLat = cleanLine[12:22]
+			cleanLat = cleanLat.strip()
+			
+			cleanLong = cleanLine[29:41]
+			cleanLong = cleanLong.strip()
+
+			lats.append(cleanLat)
+			longs.append(cleanLong)
+
+df = pd.DataFrame(columns = ['RecordID', 'Trackpoint', 'Lat', 'Long', 'Elevation', 'Date'])
+df['RecordID'] = recordID
 df['Trackpoint'] = points
+df['Lat'] = lats
+df['Long'] = longs
 
 print df
